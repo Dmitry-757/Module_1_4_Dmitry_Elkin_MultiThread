17ec11719db24a4012443d554ea67ad654811f9d
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2022-12-13 18:42:53 +0300

    ...

--- src/main/java/com/Dmitry_Elkin/cdlTest.java
+++ src/main/java/com/Dmitry_Elkin/cdlTest.java
@@ -4,11 +4,12 @@ import java.util.concurrent.CountDownLatch;
 
 public class cdlTest {
     public static void main(String[] args) {
-        CountDownLatch latch = new CountDownLatch(90);
+        CountDownLatch latch = new CountDownLatch(99);
         System.out.println("start main thread");
 
         System.out.println("start thread");
         new Thread(new MyThread(latch)).start();
+        new Thread(new MyThread2(latch)).start();
 
         try {
             latch.await();
@@ -34,6 +35,28 @@ class MyThread implements Runnable {
         for (int i = 0; i < 100; i++) {
             System.out.println("inner thread is working "+i);
             cdl.countDown();
+        }
+    }
+}
+
+class MyThread2 implements Runnable {
+    CountDownLatch cdl;
+
+    public MyThread2(CountDownLatch cdl) {
+        this.cdl = cdl;
+    }
+
+    @Override
+    public void run() {
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("current thread is " + Thread.currentThread().getName());
+        System.out.println("getCount = " + cdl.getCount());
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName()+ " is working "+i);
         }
     }
 }
